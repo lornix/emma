c21956aac60b2b3fff3effc27f9d91f5087169b3
L Nix <lornix@lornix.com>
2011-10-17 03:19:08 -0500

    working thus far

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ using namespace std;
 
 int main(int argc,const char* argv[])
 {
-    // parse options, does not return if no filenaem given
+    // parse options, does not return if no filename given
     options opts(argc,&argv[0]);
 
     // load in file data
@@ -34,20 +34,23 @@ int main(int argc,const char* argv[])
         cout << i << ": " << "\n";
         cout << "  Type: " << hexval0x(elf32.prg_headers[i]->p_type) << elf32.show_prg_type(i) << "\n";
         cout << "Offset: " << hexval0x(elf32.prg_headers[i]->p_offset,8);
+        int count=0;
         for (unsigned int j=1; j<elf32.sec_headers.size(); j++) {
             if (elf32.sec_headers[j]->sh_offset>=elf32.prg_headers[i]->p_offset) {
                 if (elf32.sec_headers[j]->sh_offset<(elf32.prg_headers[i]->p_offset+elf32.prg_headers[i]->p_memsz)) {
+                    if (count==0) cout << "\n\t\t";
                     cout << " (" << elf32.sec_name(j) << "=" << hexval0x(elf32.sec_headers[j]->sh_offset) << ")";
+                    count=(count+1)%4;
                 }
             }
         }
         cout <<"\n";
         cout << " Vaddr: " << hexval0x(elf32.prg_headers[i]->p_vaddr,8) << "\n";
         cout << " Paddr: " << hexval0x(elf32.prg_headers[i]->p_paddr,8) << "\n";
-        cout << "Pfsize: " << hexval0x(elf32.prg_headers[i]->p_filesz) << "\n";
-        cout << "Pmemsz: " << hexval0x(elf32.prg_headers[i]->p_memsz) << "\n";
-        cout << " Flags: " << hexval0x(elf32.prg_headers[i]->p_flags) << elf32.show_prg_flags(i) << "\n";
-        cout << " Align: " << hexval0x(elf32.prg_headers[i]->p_align) << "\n";
+        cout << "Pfsize: " << hexval0x(elf32.prg_headers[i]->p_filesz)  << "\n";
+        cout << "Pmemsz: " << hexval0x(elf32.prg_headers[i]->p_memsz)   << "\n";
+        cout << " Flags: " << hexval0x(elf32.prg_headers[i]->p_flags)   << elf32.show_prg_flags(i) << "\n";
+        cout << " Align: " << hexval0x(elf32.prg_headers[i]->p_align)   << "\n";
         cout << "\n";
     }
     cout << "\n";
