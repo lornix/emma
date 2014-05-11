9c88ea1a3381e2384e962b00883fb7813e1e566c
Loni Nix <lornix@lornix.com>
2014-05-11 05:49:34 -0700

    Tinkering with tst32.s

--- test/tst32.s
+++ test/tst32.s
@@ -1,129 +1,133 @@
 #
 	.text
-	.globl	_start
+	.global	_start
 #
+	.func	_start
 _start:
 	push	%rbp
 	mov	%rsp,%rbp
-	sub	$16,%rsp
 #	==============================
-	mov	$12,%rax	# brk
-	xor	%rdi,%rdi	#request 0 bytes
+	mov	$12,%rax		# SC brk
+	xor	%rdi,%rdi		# request 0 bytes
 	syscall
-	movl	$0x2d6b7262,msgstart
+	movl	$0x2d6b7262,(msgstart)	# brk-
 	call	tohex
 #	==============================
-	mov	$handle_alarm,%rax
-	mov	%rax,_handler
-	movl	$0x6c646e68,msgstart
-	call	tohex
-	mov	$sigstruct,%rax
-	movl	$0x63727473,msgstart
-	call	tohex
-#	==============================
-	mov	$128,%rcx
-	push	%rcx
-tryagain:
-	mov	$13,%rax	#sigaction
-	mov	$14,%rdi
-	mov	$sigstruct,%rsi
-	mov	$0,%rdx
+	mov	$13,%rax		# SC sigaction
+	mov	$10,%rdi		# SIGUSR1
+	mov	$sigstruct,%rsi		# new sigaction struct *
+	mov	$0,%rdx			# old sigaction struct *
+	mov	$8,%r10			# size of sigset in 64bit units?
 	syscall
-	movl	$0x61676973,msgstart
-	test	%rax,%rax
-	jnz	nogood
-#	==============================
-	pop	%rcx
-	push	%rcx
-	shl	$16,%rcx
-	and	$0xffff,%rax
-	or	%rcx,%rax
-	call	tohex
-nogood:
-	pop	%rcx
-	sub	$1,%rcx
-	jnc	tryagain
-#	==============================
-	mov	$37,%rax	#alarm
-	mov	$1,%rdi		#delay
-	syscall
-	movl	$0x6d726c61,msgstart
+	movl	$0x61676973,(msgstart)	# siga
 	call	tohex
 #	==============================
-	mov	$34,%rax	#pause
-	syscall
-#	==============================
-	movl	$0x73756170,msgstart
-	call	tohex
+	mov	$62,%rax		# SC kill
+	mov	$0,%rdi			# my process group?
+	mov	$10,%rsi		# signal == SIGUSR1
+# disabled for now, causes termination
+#	syscall
 #	==============================
 	mov	$23,%rax
-	movl	$0x2d646e72,msgstart
+	movl	$0x2d646e72,(msgstart)	# rnd-
 	call	tohex
 #	==============================
-	pop	%rsp
-	mov	$60,%rax		#exit
-	mov	$0,%rdi
+	pop	%rbp
+	mov	$60,%rax		# SC exit
+	mov	$13,%rdi
 	syscall
 	hlt
+	.endfunc
 #	==============================
+	.func	handle_alarm
 handle_alarm:
-	movl	$0x52544E49,msgstart+4
-	mov	%rdi,%rax
-	call	tohex
+	push	%rbp
+	mov	%rsp,%rbp
+	movl	$0x52544E49,(msgstart+4)	# INTR
+	pop	%rbp
 	ret
+	.endfunc
 #	==============================
+	.func	restorer
+restorer:
+	push	%rbp
+	mov	%rsp,%rbp
+	pop	%rbp
+	ret
+	.endfunc
+#	==============================
+	.func	tohex,tohex
 tohex:
+	push	%rbp
+	mov	%rsp,%rbp
+	push	%rbx
 	push	%rax
-	mov	$msgstart,%rbx
-	movl	$0x2b2b2b2b, 4(%rbx,1)
-	movl	$0x2b2b2b2b, 8(%rbx,1)
-	movl	$0x2b2b2b2b,12(%rbx,1)
-	movl	$0x2b2b2b2b,16(%rbx,1)
+	movl	$0x20202020,(msgval)
+	movl	$0x20202020,(msgval+ 4)
+	movl	$0x20202020,(msgval+ 8)
+	movl	$0x20202020,(msgval+12)
 	mov	$msgend,%rbx
-_l1:
+1:
 	push	%rax
 	and	$0x0f,%al
 	or	$0x30,%al
 	cmp	$0x3a,%al
-	jl	_l2
+	jl	2f
 	add	$39,%al
-_l2:
+2:
 	dec	%rbx
 	mov	%al,(%rbx)
 	pop	%rax
 	shr	$4,%rax
-	jnz	_l1
+	jnz	1b
 #
-	mov	$1,%rax		#write
-	mov	$1,%rdi		#fd=1, stdout
+	push	%rdi
+	push	%rsi
+	push	%rdx
+	mov	$1,%rax			# SC write
+	mov	$1,%rdi			# fd=1, stdout
 	mov	$msgstart,%rsi
-	mov	$msgend-msgstart+1,%rdx
+	mov	$(msgend-msgstart+1),%rdx
 	syscall
+	pop	%rdx
+	pop	%rsi
+	pop	%rdi
 #
 	pop	%rax
+	pop	%rbx
+	pop	%rbp
 	ret
+	.endfunc
 #
 #
 	.data
 	.align 4
 #
 msgstart:
-	.ascii	"----0000000000000000"
+	.ascii	"----"
+	.ascii	" "
+msgval:
+	.ascii	"----------------"
 msgend:
 	.byte	0x0a
 #
 	.align 16
 sigstruct:
-_handler:
-	.quad	0
-_mask:
-	.quad	0,0,0,0,0,0,0,0
-	.quad	0,0,0,0,0,0,0,0
-_flags:
+sa_handler:	# +0, 8 bytes
+	.quad	handle_alarm
+sa_mask:	# +8, 128 bytes
+	.quad	0x200,0,0,0,0,0,0,0
+	.quad	00000,0,0,0,0,0,0,0
+sa_flags:	# +136, 4 bytes
 	.long	0
-_other:
-	.long	0
-	.long	0
-	.long	0
-_strend	= .
+sa_restorer:	# +140, 8 bytes
+	.quad	restorer
+endofstat:
+	.long	-1
+#
+# Addr of sa: 7fffb12d2f70
+# Addr of sa.sa_handler:  0x00 (8 bytes)
+# Addr of sa.sa_mask:     0x08 (128 bytes)
+# Addr of sa.sa_flags:    0x88 (4 bytes)
+# Addr of sa.sa_restorer: 0x90 (8 bytes)
 #
